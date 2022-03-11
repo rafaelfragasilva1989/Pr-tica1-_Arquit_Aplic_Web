23beacd226bffad89dc703463ba92cc7d34aabe9
rafaelfragasilva1989 <82422566+rafaelfragasilva1989@users.noreply.github.com>
2022-03-10 23:39:22 -0300

    Delete src directory

--- src/controladores/banks.js
+++
@@ -1,7 +0,0 @@
-module.exports.index = function (application, req, res) {
-    var banksModel = new application.src.models.banks()
-
-    banksModel.getBanks(function (err, result) {
-        res.render('banks/index', { banks: result })
-    })
-}
